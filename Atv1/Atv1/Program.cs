098ff105e5d751a22333da9c6c8f74403a8dab84
Patrick Assugeni <126170188+patrick1408@users.noreply.github.com>
2023-08-17 10:22:30 -0300

    Add files via upload

---
+++ Atv1/Atv1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CalculoParcelas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de Compra Parcelada");
+
+            Console.Write("Digite o valor da compra: ");
+            double valorCompra = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de parcelas: ");
+            int quantidadeParcelas = Convert.ToInt32(Console.ReadLine());
+
+            double valorParcela;
+            double valorFinal;
+
+            if (quantidadeParcelas <= 1)
+            {
+                valorParcela = valorCompra * 0.9;
+                valorFinal = valorParcela;
+            }
+            else if (quantidadeParcelas <= 5)
+            {
+                valorParcela = valorCompra / quantidadeParcelas;
+                valorFinal = valorCompra;
+            }
+            else
+            {
+                valorParcela = (valorCompra * 1.07) / quantidadeParcelas;
+                valorFinal = valorCompra * 1.07;
+            }
+
+            Console.WriteLine("\nDetalhes da Compra:");
+            Console.WriteLine($"Valor da compra: R$ {valorCompra:F2}");
+            Console.WriteLine($"Quantidade de parcelas: {quantidadeParcelas}");
+            Console.WriteLine($"Valor das parcelas: R$ {valorParcela:F2}");
+            Console.WriteLine($"Valor final da compra: R$ {valorFinal:F2}");
+        }
+    }
+}
